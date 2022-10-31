a14c6ddeec2a1c1531eb750e6e7cb1a7394b8c2a
SJohnmith <smith795052@gmail.com>
2022-10-30 21:38:33 -0400

    Header_Buttons

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-// console.log(window);
+// Self-Invoking
+(function (global){
+    var g = {};
+    
+    function myFcn(){
+        document.getElementById("demo").innerHTML = "Are we targetting this ID? We got the paragraph";
+    };
+
+    // On page loaded (before images or CSS)
+    document.addEventListener("DOMContentLoaded", myFcn);
+
+    global.$g = g;
+})(window);
