a9a08b60f555221c46cd18e222e1844b03ecce5b
SJohnmith <smith795052@gmail.com>
2023-10-08 14:23:54 -0400

    Redesign

--- js/script.js
+++ js/script.js
@@ -11,3 +11,24 @@
 
     global.$g = g;
 })(window);
+
+
+
+const navLinksEls = document.querySelectorAll(".nav_link");
+const slidesEls = document.querySelectorAll(".slides");
+
+let currentSlide = "one";
+window.addEventListener('scroll', () => {
+    slidesEls.forEach(slidesEl => {
+        if (window.scrollY >= slidesEl.offsetTop){
+            currentSlide = slidesEl.id;
+        }
+    });
+
+    navLinksEls.forEach(navLinksEl => {
+        if (navLinksEl.href.includes(currentSlide)) {
+            document.querySelector('.active').classList.remove('active');
+            navLinksEl.classList.add('active');
+        }
+    });
+});
