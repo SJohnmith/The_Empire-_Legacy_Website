c9bc8906b2a4493f39bedd776494e34266811136
SJohnmith <smith795052@gmail.com>
2023-10-09 13:48:27 -0400

    Test for Mobile

--- js/script.js
+++ js/script.js
@@ -49,3 +49,11 @@ window.onscroll = () => {
         };
     });
 };
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navBarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggleButton.addEventListener('click', () => {
+    navBarLinks.classList.toggle('active');
+});
